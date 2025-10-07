1821d86439f9ebd6b9231a88e67a1d34676775c0
VuDucTriJoseph <vutrilkvn89@gmail.com>
2025-10-07 20:47:51 +0700

    finished

--- components/contactForm.jsx
+++ components/contactForm.jsx
@@ -30,45 +30,54 @@ function ContactForm() {
       <form id="form-contact" onSubmit={handleSubmit(onSubmit)} noValidate>
         <h2>Contact Us</h2>
         <div className="name">
-          <label htmlFor="name-firstName">First Name</label>
-          <input
-            type="text"
-            //   name="firstName" become register
-            id="name-firstName"
-            aria-required="true"
-            aria-invalid={errors.firstName ? "true" : "false"}
-            aria-describedby="name-firstName--required"
-            autoComplete="given-name"
-            {...register("firstName", { required: "This field is required" })}
-          />
-          {errors.firstName && (
-            <p className="arlet" id="name-firstName--required">
-              {errors.firstName.message}
-            </p>
-          )}
-
-          <label htmlFor="name-lastName">Last Name</label>
-          <input
-            type="text"
-            //   name="lastName"
-            id="name-lastName"
-            //   required
-            aria-required="true"
-            aria-invalid={errors.lastName ? "true" : "false"}
-            aria-describedby="name-lastName--required"
-            autoComplete="family-name"
-            {...register("lastName", {
-              required: "This field is required",
-            })}
-          />
-          {errors.lastName && (
-            <p className="arlet" id="name-lastName--required">
-              {errors.lastName.message}
-            </p>
-          )}
+          <div>
+            <label htmlFor="name-firstName">
+              First Name <span aria-hidden="true">*</span>
+            </label>
+            <input
+              type="text"
+              //   name="firstName" become register
+              id="name-firstName"
+              aria-required="true"
+              aria-invalid={errors.firstName ? "true" : "false"}
+              aria-describedby="name-firstName--required"
+              autoComplete="given-name"
+              {...register("firstName", { required: "This field is required" })}
+            />
+            {errors.firstName && (
+              <p className="arlet" id="name-firstName--required">
+                {errors.firstName.message}
+              </p>
+            )}
+          </div>
+          <div>
+            <label htmlFor="name-lastName">
+              Last Name <span aria-hidden="true">*</span>
+            </label>
+            <input
+              type="text"
+              //   name="lastName"
+              id="name-lastName"
+              //   required
+              aria-required="true"
+              aria-invalid={errors.lastName ? "true" : "false"}
+              aria-describedby="name-lastName--required"
+              autoComplete="family-name"
+              {...register("lastName", {
+                required: "This field is required",
+              })}
+            />
+            {errors.lastName && (
+              <p className="arlet" id="name-lastName--required">
+                {errors.lastName.message}
+              </p>
+            )}
+          </div>
         </div>
         <div className="email">
-          <label htmlFor="email">Email Address</label>
+          <label htmlFor="email">
+            Email Address <span aria-hidden="true">*</span>
+          </label>
           <input
             type="email"
             //   name="email"
@@ -102,29 +111,33 @@ function ContactForm() {
           aria-describedby="query-selected"
           aria-invalid={errors.queryType ? "true" : "false"}
         >
-          <legend>Query Type</legend>
-          <label className="tick-box">
-            <input
-              type="radio"
-              //   name="queryType"
-              value="general"
-              {...register("queryType", {
-                required: "Please select a query type",
-              })}
-              id="query-general"
-            />
-            General Enquiry
-          </label>
-          <label className="tick-box">
-            <input
-              type="radio"
-              //   name="queryType"
-              value="support"
-              id="query-support"
-              {...register("queryType")}
-            />
-            Support Request
-          </label>
+          <legend>
+            Query Type <span aria-hidden="true">*</span>
+          </legend>
+          <div className="query">
+            <label className="tick-box query-type">
+              <input
+                type="radio"
+                //   name="queryType"
+                value="general"
+                {...register("queryType", {
+                  required: "Please select a query type",
+                })}
+                id="query-general"
+              />
+              General Enquiry
+            </label>
+            <label className="tick-box query-type">
+              <input
+                type="radio"
+                //   name="queryType"
+                value="support"
+                id="query-support"
+                {...register("queryType")}
+              />
+              Support Request
+            </label>
+          </div>
 
           {errors.queryType && (
             <p className="arlet" id="query-selected">
@@ -133,7 +146,9 @@ function ContactForm() {
           )}
         </fieldset>
         <div className="text-mes">
-          <label htmlFor="message">Message</label>
+          <label htmlFor="message">
+            Message <span aria-hidden="true">*</span>
+          </label>
           <textarea
             //   name="message"
             id="message"
@@ -165,7 +180,8 @@ function ContactForm() {
                   "To submit this form, please consent to being contacted",
               })}
             />
-            I consent to being contacted by the team
+            I consent to being contacted by the team{" "}
+            <span aria-hidden="true">*</span>
           </label>
           {errors.accept && (
             <p className="arlet" id="accept-required">
@@ -174,7 +190,7 @@ function ContactForm() {
           )}
         </div>
 
-        <input type="submit" />
+        <input type="submit" className="btn" />
       </form>
       {isSubmmited && <Message />}
       {/* <Message /> */}
